79b5c16441bb660d9a62bd094d02c1deffcb22c7
samirpulatov <samirpulatov42@gmail.com>
2025-04-16 21:08:30 +0200

    Modify output

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,30 @@ import java.util.Scanner;
 
 
 public class Main {
+
+
+    public static void start(){
+        String GREEN = "\u001B[32m";
+         String RESET = "\u001B[0m";
+        int size = 29;
+        char[] length = new char[size];
+        for(int i=0;i<size;i++) {
+            length[i]='*';
+        }
+        String message = "Shunting_Yard_Calculator!";
+        System.out.print(GREEN);
+        for(char ch:length)
+            System.out.print(ch);
+        System.out.println();
+        System.out.println("* "+message+" *");
+         for(char ch:length)
+            System.out.print(ch);
+        System.out.println();
+        System.out.print(RESET);
+    }
+
     public static void main(String[] args) {
+        start();
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter your expression: ");
         String userInput = scanner.nextLine();
